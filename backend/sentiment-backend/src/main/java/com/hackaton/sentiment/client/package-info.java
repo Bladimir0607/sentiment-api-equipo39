6c580b97466f1aaea3f7cdafe108f95ed87490bc
Bladimir <vladimirantonioventura6@gmail.com>
2026-01-27 22:53:54 -0400

    updated

--- backend/sentiment-backend/src/main/java/com/hackaton/sentiment/client/package-info.java
+++ backend/sentiment-backend/src/main/java/com/hackaton/sentiment/client/package-info.java
@@ -1,9 +1,6 @@
 package com.hackaton.sentiment.client;
-
-/**
- * Clientes externos de la aplicación.
+ /* Clientes externos de la aplicación.
  * <p>
  * Este paquete contiene las clases responsables de comunicarse
  * con servicios externos o APIs de terceros utilizados por el sistema.
  */
-
